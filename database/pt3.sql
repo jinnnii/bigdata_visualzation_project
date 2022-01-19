8c98c1655dab1eeaeaaab6494870194cdb167155
jinnnii <jinnnii@naver.com>
2022-01-19 11:41:42 +0900

    Refactoring : 22-01-19. DB's test3

--- database/pt3.sql
+++ database/pt3.sql
@@ -5,23 +5,24 @@ WHERE PAY>=50000000;
 
 --2. emp2 테이블에서 이름, 급여, 보너스, 세금을 출력하라, 보너스는 급여의 50%, 세금은 급여의 10%로 계산하여 출력하고 
 --보너스와 세금 필드의 별명은 각각 “보너스”, “세금”으로 지정한다. 
-SELECT NAME, PAY, PAY*0.5 AS BONUS, PAY*0.1 AS TAX
+SELECT NAME, TO_CHAR(PAY,'999,999,999,999'), PAY*0.5 AS BONUS, PAY*0.1 AS TAX
 FROM EMP2;
 
 --3. emp2 테이블에서 취미(hobby)가 “Music, Climb, Swim”인 사람의 이름과 취미를 출력하라.
 SELECT NAME, HOBBY
 FROM EMP2
-WHERE HOBBY IN(INITCAP('MUSIC'),INITCAP('CLIMB'),INITCAP('SWIM'));
+-- WHERE HOBBY IN(INITCAP('MUSIC'),INITCAP('CLIMB'),INITCAP('SWIM'));
+WHERE UPPER(HOBBY) IN ('MUSIC','CLIMB','SWIM');
 
 --4. 교수(professor) 테이블에서 연봉이 5000 이상인 교수의 name, pay, bonus, 연봉(pay*12+bonus)를 출력하라. 
-SELECT NAME, PAY, BONUS, PAY*12+BONUS AS YEAR_PAY
+SELECT NAME, PAY, BONUS, PAY*12+NVL(BONUS,0) AS YEAR_PAY
     FROM PROFESSOR
-    WHERE PAY*12+BONUS>=5000;
+    WHERE PAY*12+NVL(BONUS,0)>=5000;
 
 --5. 교수(professor) 테이블에서 id가 “s"로 시작하는 교수의 이름(name), 아이디(id)를 출력하라. 
 SELECT NAME, ID
     FROM PROFESSOR
-    WHERE ID LIKE 'S%';
+    WHERE UPPER(ID) LIKE 'S%';
 
 --6. 교수 테이블에서 홈페이지(hpage)가 있는 교수의 이름(name), 홈페이지(hpage)를 출력하라. 
 SELECT NAME, HPAGE
