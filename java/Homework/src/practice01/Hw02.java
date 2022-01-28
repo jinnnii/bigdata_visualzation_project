6d7556065a6e856e700433aac51b63082b79ff22
jinnnii <jinnnii@naver.com>
2022-01-28 16:33:34 +0900

    Refactoring : 22-01-28. Hw02

--- java/Homework/src/practice01/Hw02.java
+++ java/Homework/src/practice01/Hw02.java
@@ -4,15 +4,20 @@ public class Hw02 {
 
 	public static void main(String[] args) {
 		for(int i=1; i<=99; i++) {
-			String str= String.valueOf(i);
+//			String str= String.valueOf(i);
 			System.out.print(i);
+			int numTen =i/10;
+			int numOne =i%10;
 			int count =0;
-			for(int j=0; j<str.length();j++) {
-				char ch = str.charAt(j);
-				if(ch=='3'||ch=='6'||ch=='9') {
-					count++;
-				}
-			}
+			
+			if(numTen!=0 && numTen%3==0) count++;
+			if(numOne!=0 && numOne%3==0) count++;
+//			for(int j=0; j<str.length();j++) {
+//				char ch = str.charAt(j);
+//				if(ch=='3'||ch=='6'||ch=='9') {
+//					count++;
+//				}
+//			}
 			if(count>0) {
 				System.out.print(" 박수");
 				for(int j=0; j<count; j++) {
